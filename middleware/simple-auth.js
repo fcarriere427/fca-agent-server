b4c0bcd3e7b6fcaa06ef84ab22ad86373d1d51f8
Florian <florian.carriere@gmail.com>
2025-04-26 09:02:15 +0200

    simplify

--- middleware/simple-auth.js
+++
@@ -1,5 +0,0 @@
-// FICHIER DÉPRÉCIÉ - Utiliser utils/auth.js à la place
-// Ce fichier est conservé pour référence et compatibilité
-const simpleAuthMiddleware = require('../utils/auth');
-
-module.exports = simpleAuthMiddleware;
